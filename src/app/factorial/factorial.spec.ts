1c78bef51d2ecc7c852d1ccb846fb7e9b16aef68
Lord17Charly <gutierrezvottecarlos@gmail.com>
2024-10-13 21:12:55 -0600

    agregar cambios

--- src/app/factorial/factorial.spec.ts
+++ src/app/factorial/factorial.spec.ts
@@ -1,33 +1,43 @@
 import { factorial } from './factorial';
 
 describe('Factorial test suite', () => {
-  it('Should return 0 if input is negative', () => {
+  it('deberia retornar 0 si el resultado es menor a 0', () => {
     const result = factorial(-1);
     expect(result).toBe(0);
   });
 
-  it('Should return 1 if input is 0', () => {
+  it('deberia retornar 1 si el factorial dado es 0', () => {
     const result = factorial(0);
     expect(result).toBe(1);
   });
 
-  it('Should return 1 if input is 1', () => {
-    const result = factorial(1);
-    expect(result).toBe(1);
-  });
-
-  it('Should return 2 if input is 2', () => {
+  it('deberia retornar 2 si el factorial es 2', () => {
     const result = factorial(2);
     expect(result).toBe(2);
   });
 
-  it('Should return 6 if input is 3', () => {
+  it('deberia retornar 6 si x es 3', () => {
     const result = factorial(3);
     expect(result).toBe(6);
   });
 
-  it('Should return 24 if input is 4', () => {
+  it('deberia retornar 24 si x es 4', () => {
     const result = factorial(4);
     expect(result).toBe(24);
+  });
+
+  it('debria retornar 120 si x es 5', () => {
+    const result = factorial(5);
+    expect(result).toBe(120);
+  });
+
+  it('debria retornar 0 si x > 15', () => {
+    const result = factorial(5);
+    expect(result).toBe(120);
+  });
+
+  it('debria retornar 0 si x es mayor a 15', () => {
+    const result = factorial(16);
+    expect(result).toBe(0);
   });
 });
