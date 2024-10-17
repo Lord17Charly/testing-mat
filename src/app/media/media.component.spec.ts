f6a42e7a13e944cca4ad39a5d831e7177cca156c
Lord17Charly <gutierrezvottecarlos@gmail.com>
2024-10-17 00:58:15 -0600

    refactor media

--- src/app/media/media.component.spec.ts
+++ src/app/media/media.component.spec.ts
@@ -1,73 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import {
-  HttpClientTestingModule,
-  HttpTestingController,
-} from '@angular/common/http/testing';
+import mediaData from './media.json';
 import { MediaComponent } from './media.component';
-import { of, throwError } from 'rxjs';
 
 describe('MediaComponent', () => {
   let component: MediaComponent;
   let fixture: ComponentFixture<MediaComponent>;
-  let httpTestingController: HttpTestingController;
+  let numerosColumna1: number[] = mediaData.columa1;
+  let numerosColumna2: number[] = mediaData.columna2;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, MediaComponent],
+      imports: [MediaComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MediaComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-
-    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should calculate the correct mean from the JSON data', () => {
-    const mockData = {
-      column1: [160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503],
-    };
-
-    spyOn(component['http'], 'get').and.returnValue(of(mockData));
-
-    // Llamar a la función de calcular la media
-    component.calculateMeanFromFile('assets/colum1.json');
-
-    // Verificar que la media calculada sea la correcta
-    expect(component.mean).toBeCloseTo(550.6, 1);
+  it('should return media = 550.6 for the given input (columna1)', () => {
+    const media = component.obtenerMedia(numerosColumna1);
+    expect(media).toBeCloseTo(550.6, 1);
   });
 
-  it('should handle empty data and set mean to null', () => {
-    const mockData = { column1: [] };
-
-    spyOn(component['http'], 'get').and.returnValue(of(mockData));
-
-    // Llamar a la función de calcular la media
-    component.calculateMeanFromFile('assets/colum1.json');
-
-    // Verificar que la media sea null (no hay datos)
-    expect(component.mean).toBeNaN();
-  });
-
-  it('should handle HTTP error gracefully', () => {
-    spyOn(component['http'], 'get').and.returnValue(
-      throwError('Error al leer el archivo')
-    );
-
-    // Llamar a la función de calcular la media
-    component.calculateMeanFromFile('assets/colum1.json');
-
-    // Verificar que la media sea null y se registre un error
-    expect(component.mean).toBeNull();
-  });
-
-  it('should correctly calculate the mean', () => {
-    const numbers = [10, 20, 30];
-    const mean = component.calculateMean(numbers);
-    expect(mean).toBe(20);
+  it('should return media = 60.32 for the given input (columna2)', () => {
+    const media = component.obtenerMedia(numerosColumna2);
+    expect(media).toBeCloseTo(60.32, 2);
   });
 });
--- src/app/media/media.component.ts
+++ src/app/media/media.component.ts
@@ -1,41 +1,15 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-media',
   standalone: true,
   imports: [],
   templateUrl: './media.component.html',
-  styleUrls: ['./media.component.css'],
+  styleUrl: './media.component.css',
 })
 export class MediaComponent {
-  public mean: number | null = null;
-
-  constructor(private http: HttpClient) {
-    this.calculateMeanFromFile('assets/colum1.json');
-  }
-
-  readDataFromFile(fileName: string): void {
-    this.http.get<any>(fileName).subscribe(
-      (data) => {
-        this.calculateMeanFromData(data.column1);
-      },
-      (error) => {
-        console.error('Error al leer el archivo:', error);
-      }
-    );
-  }
-
-  calculateMean(numbers: number[]): number {
-    const total = numbers.reduce((acc, curr) => acc + curr, 0);
-    return total / numbers.length;
-  }
-
-  calculateMeanFromFile(fileName: string): void {
-    this.readDataFromFile(fileName);
-  }
-
-  calculateMeanFromData(numbers: number[]): void {
-    this.mean = this.calculateMean(numbers);
+  obtenerMedia(numeros: number[]): number {
+    const suma = numeros.reduce((a, b) => a + b, 0);
+    return suma / numeros.length;
   }
 }
