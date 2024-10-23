d14d2af50e93ea0456157447ad22b25ccc75a8ec
Lord17Charly <gutierrezvottecarlos@gmail.com>
2024-10-23 12:56:15 -0600

    eliminacion de archivos

--- src/app/linear_regresion/linear-regression/linear-regression.component.spec.ts
+++ src/app/linear_regresion/linear-regression/linear-regression.component.spec.ts
@@ -31,36 +31,6 @@ describe("LinearRegressionComponent", () => {
     expect(component).toBeTruthy();
   });
 
-  // Prueba para la sumatoria de x * y
-  it("retorna la sumatoria de x * y", () => {
-    const result = component.procesarArrays(test1, test2);
-    expect(result).toEqual(4303108);
-  });
-
-  // Prueba para la media de x
-  it("retorna la media de x", () => {
-    const result = component.mediax(test1);
-    expect(result).toEqual(382.8);
-  });
-
-  // Prueba para la media de y
-  it("retorna la media de y", () => {
-    const result = component.mediay(test2);
-    expect(result).toEqual(638.9);
-  });
-
-  // Prueba de la parte superior de la fórmula de la pendiente
-  it("parte superior", () => {
-    const result = component.Pendiente(test1, test2);
-    expect(result).toBeCloseTo(1857398.8, 1);
-  });
-
-  // Prueba de la parte inferior de la fórmula de la pendiente
-  it("parte inferior", () => {
-    const result = component.Pendiente2(test1, test2);
-    expect(result).toEqual(1074925.6);
-  });
-
   // Prueba para la pendiente verdadera
   it("Should return B1=1.7279 with the dataset Data_Test1", () => {
     const result = component.Pendienteverdadera(test1, test2);
